4f694abccd34e567087b6f264676109cebc933cc
ScorchingShade <ankushors789@gmail.com>
2021-09-17 03:24:03 +0530

    added complete file based routing setup with 404.sj

--- pages/client/index.js
+++ pages/client/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
+import Link from 'next/link'
+import router from 'next/router';
+
 function ClientsPage() {
+    const clients=[
+        {id:"ak",name:"sh"},
+        {id:"pk",name:"MJ"},
+
+    ];
+
+    const loadProjectHandler=()=>{
+        router.push("/client/ak/projectA");
+    }
+  
     return (
         <div>
             <h1>ClientsPage</h1>
+            <ul>
+                {clients.map((client) =><li key={client.id}>
+                    <Link href={{
+                        pathname:"/client/[id]",
+                        query:{id:client.id}
+                    }}>{client.name}</Link>
+
+                </li>)}
+            </ul>
+
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
 
 function IndexPage() {
   return (
     <div>
       Hello
+      <ul>
+        <li>
+          <Link replace href="/about">About</Link>
+        </li>
+
+        <li>
+          <Link replace href="/client">Clients</Link>
+        </li>
+      </ul>
     </div>
   )
 }
