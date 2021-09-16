f4a74d787e96d6c97a1c4471e69f1658b9fe3f57
ScorchingShade <ankushors789@gmail.com>
2021-09-17 03:24:25 +0530

    added complete file based routing setup with 404.js

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Error() {
+    return (
+        <div>
+            You are at a wrong page man!
+            
+        </div>
+    )
+}
+
+export default Error
